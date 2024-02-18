fb2b76c91a3f9c823e97138b2f3860bbad0b1464
Rafael Santos <osmarditto@gmail.com>
2024-02-18 19:17:50 -0300

    MemberConfiguration IEntityTypeConfiguration
    AppDBContext

---
+++ CleanArch.Infrastruct/Context/AppDBContext.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Entities;
+using CleanArch.Infrastruct.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infrastruct.Context
+{
+  internal class AppDBContext : DbContext
+  {
+    public AppDBContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Member> Members { get; set; }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.ApplyConfiguration(new MemberConfiguration());
+    }
+  }
+}
