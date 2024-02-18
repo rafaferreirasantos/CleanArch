1b3b2441465f5061b4bced7e61626830db6ca37d
Rafael Santos <osmarditto@gmail.com>
2024-02-18 17:05:11 -0300

    Entity abstract class; Member Entity;

---
+++ CleanArch.Domain/Entities/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Entities
+{
+  internal sealed class Member : Entity
+  {
+        public string? FirstName { get; private set; }
+        public string? LastName { get; private set; }
+        public string? Gender { get; private set; }
+        public string? Email { get; private set; }
+        public bool? IsActive { get; set; }
+    }
+}
