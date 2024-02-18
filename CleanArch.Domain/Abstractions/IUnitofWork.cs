8df8efb94e2a37592e0b7db418524217cc4bd5b7
Rafael Santos <osmarditto@gmail.com>
2024-02-18 17:40:11 -0300

    IUnitofWork interface

---
+++ CleanArch.Domain/Abstractions/IUnitofWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Abstractions
+{
+  internal interface IUnitofWork
+  {
+    IMemberRepository MemberRepository { get; }
+    Task CommitAsync();
+  }
+}
