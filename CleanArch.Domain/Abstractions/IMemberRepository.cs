bee3200607477762135884421f5b978d3e020a7e
Rafael Santos <osmarditto@gmail.com>
2024-02-18 17:28:23 -0300

    IMemberRepository Interface

---
+++ CleanArch.Domain/Abstractions/IMemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using CleanArch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Abstractions
+{
+  internal interface IMemberRepository
+  {
+    Task<IEnumerable<Member>> GetAll();
+    Task<Member> GetById(int idMember);
+    Task<Member> Add(Member member);
+    Task<Member> Update(Member member);
+    Task Delete(int idMember);
+  }
+}
