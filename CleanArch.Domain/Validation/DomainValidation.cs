0f291c01644f540f7f55e9811cebd83210cd59ad
Rafael Santos <osmarditto@gmail.com>
2024-02-18 17:07:51 -0300

    DomainValidation Exception class

---
+++ CleanArch.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Domain.Validation
+{
+  internal class DomainValidation : Exception
+  {
+    public DomainValidation(string? message) : base(message)
+    {
+    }
+  public static void When(bool hasError, string message)
+    {
+      if(hasError)
+        throw new DomainValidation(message);
+    }
+  }
+}
